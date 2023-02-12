b063bf986c2d1de31ea47b73375d79d38d0073cc
Gabriel <gabriellopes112009@gmail.com>
2023-02-12 12:57:58 -0300

    configuração para gerar id por instância, e endpoint para mostrar a porta que a aplicação está rodando

--- src/main/java/br/com/alurafood/pedidos/controller/PedidoController.java
+++ src/main/java/br/com/alurafood/pedidos/controller/PedidoController.java
@@ -4,6 +4,7 @@ import br.com.alurafood.pedidos.dto.PedidoDto;
 import br.com.alurafood.pedidos.dto.StatusDto;
 import br.com.alurafood.pedidos.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -56,5 +57,10 @@ public class PedidoController {
 
             return ResponseEntity.ok().build();
 
+        }
+
+        @GetMapping("/porta")
+        public String retornaPorta(@Value("${local.server.port}") String porta){
+            return String.format("<h1>Requisição respondida pela instância executando na porta: %s<h1>", porta);
         }
 }

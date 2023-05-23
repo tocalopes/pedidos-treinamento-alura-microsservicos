d530fb724dfb9e267ff10a084f8ee4375ad2f0ec
Gabriel <gabriellopes112009@gmail.com>
2023-05-23 17:52:02 -0300

    alteração do projeto para rodar com banco de dados mysql docker

--- src/main/java/br/com/alurafood/pedidos/PedidosApplication.java
+++ src/main/java/br/com/alurafood/pedidos/PedidosApplication.java
@@ -2,11 +2,11 @@ package br.com.alurafood.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableFeignClients
 public class PedidosApplication {
 
